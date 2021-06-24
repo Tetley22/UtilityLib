bce5aae935b6dd4e1253bbbc705d13674c3a1d1d
Brad Walton <pj_sammie@hotmail.com>
2021-06-24 15:19:32 -0700

    Add project files.

---
+++ Logger/SimpleLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logger
+{
+    public class SimpleLogger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine($"Message: {message}");
+        }
+    }
+}
