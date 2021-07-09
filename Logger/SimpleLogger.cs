68453c61b97038d5d2fa0828d0de7125ecad69c3
Brad Walton <pj_sammie@hotmail.com>
2021-07-09 12:06:40 -0700

    changed something

--- Logger/SimpleLogger.cs
+++ Logger/SimpleLogger.cs
@@ -4,11 +4,11 @@ namespace Logger
 {
     public class SimpleLogger
     {
-        static int counter = 0;
+        static int counter = 1;
         
         public static void Log(string message, string? caller)
         {
-            Console.WriteLine($"[{counter}:{caller}] Message: {message}");
+            Console.WriteLine($"[{counter}:{caller}] {message}");
             ++counter;
         }
     }
