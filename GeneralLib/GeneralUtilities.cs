f5f19f74722735ffb62b7671d1856944518e7a68
Brad Walton <github@greatergreen.com>
2021-08-24 21:43:12 -0700

    Added new project for general utility stuff.

---
+++ GeneralLib/GeneralUtilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralLib
+{
+    public class GeneralUtilities
+    {
+        public static string ToOrdinalString(int val)
+        {
+            string extension = "th";
+
+            // last 2 digits
+            int lastTwoDigits = val % 100;
+
+            if (lastTwoDigits < 11 || lastTwoDigits > 13)
+            {
+                // last digit
+                int lastDigit = lastTwoDigits % 10;
+
+                extension = (lastDigit) switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+            }
+
+            return $"{val}{extension}";
+        }
+    }
+}
